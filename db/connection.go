9d0478077ed1069cfe9955a4f295c0d296cbe5ff
Kieran Finn <kieran.finn@hotmail.com>
2025-04-13 21:13:57 +0100

    move read config to main method

--- db/connection.go
+++ db/connection.go
@@ -2,8 +2,6 @@ package db
 
 import (
 	"database/sql"
-	"os"
-	"strings"
 )
 
 func GetDB() (*sql.DB, error){
@@ -20,21 +18,4 @@ func GetDB() (*sql.DB, error){
 	}
 
 	return db, nil
-}
-
-func ReadDBConfig(filename string) (map[string]string, error){
-	rawData, err := os.ReadFile(filename)
-	if err!=nil{
-		return nil, err
-	}
-
-	out:=make(map[string]string)
-
-	lines:=strings.Split(string(rawData), "\n")
-	for _, line:= range lines{
-		words:=strings.Split(line," ")
-		out[words[0]]=words[1]
-	}
-
-	return out, nil
 }
--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
-	"flatchecker-scheduler/db"
+	"os"
+	"strings"
 )
 
 func main(){
-	config, err:=db.ReadDBConfig("C:\\Users\\kiera\\flatchecker\\flatchecker-database\\setup\\db_credentials.txt")
+	config, err:=ReadConfig("C:\\Users\\kiera\\flatchecker\\flatchecker-database\\setup\\db_credentials.txt")
 	if err!=nil{
 		panic(fmt.Sprintf("error reading config: %v",  err))
 	}
 	fmt.Println(config)
 	fmt.Println("Hello, World!")
+}
+
+
+
+func ReadConfig(filename string) (map[string]string, error){
+	rawData, err := os.ReadFile(filename)
+	if err!=nil{
+		return nil, err
+	}
+
+	out:=make(map[string]string)
+
+	lines:=strings.Split(string(rawData), "\n")
+	for _, line:= range lines{
+		words:=strings.Split(line," ")
+		out[words[0]]=words[1]
+	}
+
+	return out, nil
 }
