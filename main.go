f770b73fc9004b47f839c572d9733b6f4352f7ee
Kieran Finn <kieran.finn@hotmail.com>
2025-04-13 21:12:51 +0100

    read config on startup

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"flatchecker-scheduler/db"
 )
 
 func main(){
+	config, err:=db.ReadDBConfig("C:\\Users\\kiera\\flatchecker\\flatchecker-database\\setup\\db_credentials.txt")
+	if err!=nil{
+		panic(fmt.Sprintf("error reading config: %v",  err))
+	}
+	fmt.Println(config)
 	fmt.Println("Hello, World!")
 }
