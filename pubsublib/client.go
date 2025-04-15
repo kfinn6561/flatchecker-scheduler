b3d17149b93cfb6df8a47449fc5033f95d642f35
Kieran Finn <kieran.finn@hotmail.com>
2025-04-15 20:58:39 +0100

    create pubsub wrapper

---
+++ pubsublib/client.go
@@ -0,0 +1,13 @@
+package pubsublib
+
+import (
+	"context"
+
+	"cloud.google.com/go/pubsub"
+)
+
+const PROJECT_ID="flatchecker"
+
+func GetClient(ctx context.Context)(*pubsub.Client,error){
+return pubsub.NewClient(ctx, PROJECT_ID)
+}
