2af78e7bf48723fa8ea0a0965792352b82bc178b
Kieran Finn <kieran.finn@hotmail.com>
2025-04-27 15:06:14 +0100

    go fmt

--- db/get_schedules.go
+++ db/get_schedules.go
@@ -2,7 +2,7 @@ package db
 
 import "database/sql"
 
-const GET_SCHEDULE_SQL_NAME="get_schedules.sql"
+const GET_SCHEDULE_SQL_NAME = "get_schedules.sql"
 
 type GetSchedulesResponse struct {
 	ScheduleId int
@@ -10,29 +10,29 @@ type GetSchedulesResponse struct {
 }
 
 func GetSchedules(db *sql.DB) ([]GetSchedulesResponse, error) {
-	stmtString,err:=readSqlFile(GET_SCHEDULE_SQL_NAME)
-	if err!=nil {
+	stmtString, err := readSqlFile(GET_SCHEDULE_SQL_NAME)
+	if err != nil {
 		return nil, err
 	}
 
-	rows,err:=db.Query(stmtString)
-	if err!=nil {
+	rows, err := db.Query(stmtString)
+	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-    var schedules []GetSchedulesResponse
+	var schedules []GetSchedulesResponse
 
-    for rows.Next() {
-        var schedule GetSchedulesResponse
-        if err := rows.Scan(&schedule.ScheduleId, &schedule.SearchId); err != nil {
-            return schedules, err
-        }
-        schedules = append(schedules, schedule)
-    }
-    if err = rows.Err(); err != nil {
-        return schedules, err
-    }
+	for rows.Next() {
+		var schedule GetSchedulesResponse
+		if err := rows.Scan(&schedule.ScheduleId, &schedule.SearchId); err != nil {
+			return schedules, err
+		}
+		schedules = append(schedules, schedule)
+	}
+	if err = rows.Err(); err != nil {
+		return schedules, err
+	}
 
-return schedules, nil
+	return schedules, nil
 }
