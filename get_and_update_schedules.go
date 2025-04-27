e8fb6beb0d6cfa13f2020fe20e27bed6505b74b9
Kieran Finn <kieran.finn@hotmail.com>
2025-04-27 20:39:10 +0100

    get and update

---
+++ get_and_update_schedules.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"flatchecker-scheduler/db"
+	"time"
+)
+
+func GetAndUpdateSchedules(dbConn *sql.DB) ([]db.GetSchedulesResponse, error) {
+	schedules, err := db.GetSchedules(dbConn)
+	if err != nil {
+		return nil, err
+	}
+
+	requests := make([]db.UpdateScheduleRequest, len(schedules))
+	for i, schedule := range schedules {
+		request := db.UpdateScheduleRequest{
+			Id:         schedule.ScheduleId,
+			NextSearch: schedule.NextSearch.Add(time.Minute * time.Duration(schedule.SearchDelayMinutes)),
+		}
+		requests[i] = request
+	}
+
+	db.UpdateSchedules(requests, dbConn)
+	return schedules, nil
+}
