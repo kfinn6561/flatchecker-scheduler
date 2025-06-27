98d31ddbfca7b7739c09b221b2b3a384c91de506
Kieran Finn <kieran.finn@hotmail.com>
2025-06-27 19:49:38 +0100

    add base terraform

---
+++ deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_project" {
+  description = "gcp Project ID"
+  default     = "flatchecker"
+}
+
+variable "gcp_region" {
+  description = "default gcp region"
+  default     = "europe-north1"
+}
+
+variable "gcp_zone" {
+  description = "default gcp zone"
+  default     = "europe-north1-a"
+}
