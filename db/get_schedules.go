868b7c65d02818a62f0620662d3adeb8a8d06c01
Kieran Finn <kieran.finn@hotmail.com>
2025-08-26 21:13:24 +0100

    more logging

--- db/get_schedules.go
+++ db/get_schedules.go
@@ -18,12 +18,12 @@ type GetSchedulesResponse struct {
 func GetSchedules(db *sql.DB) ([]GetSchedulesResponse, error) {
 	stmtString, err := readSqlFile(GET_SCHEDULE_SQL_NAME)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error reading sql file: %v", err)
 	}
 
 	rows, err := db.Query(stmtString)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error querying database: %v", err)
 	}
 	defer rows.Close()
 
@@ -32,12 +32,12 @@ func GetSchedules(db *sql.DB) ([]GetSchedulesResponse, error) {
 	for rows.Next() {
 		var schedule GetSchedulesResponse
 		if err := rows.Scan(&schedule.ScheduleId, &schedule.SearchId, &schedule.NextSearch, &schedule.SearchDelayMinutes); err != nil {
-			return schedules, err
+			return schedules, fmt.Errorf("error scanning row: %v", err)
 		}
 		schedules = append(schedules, schedule)
 	}
 	if err = rows.Err(); err != nil {
-		return schedules, err
+		return schedules, fmt.Errorf("error iterating rows: %v", err)
 	}
 	fmt.Println(schedules)
 
